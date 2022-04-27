03f06beb71eba1456e0cf7e33d3f9b99ce1147a1
CAOXIAOYANG <1076675153@qq.com>
2022-04-27 20:43:05 +0800

    init cloud

---
+++ power/src/main/java/com/google/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.google.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com")
+@Configuration
+public class AppConfig {
+
+
+//    @Bean
+//    public TomcatServletWebServerFactory tomcat(){
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.setPort(6000);
+//        return  tomcat;
+//    }
+
+}
